8bcb844f24379bc20e0bb007b3e3f48eeee8ad0b
noppi401 <noppi623@gmail.com>
2026-05-01 01:27:10 +0900

    fix: repair task CLI parsing and delete command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-#!/usr/bin/env node
-import { CLI } from './lib/cli.js';
-
-const cli = new CLI();
-await cli.run(process.argv.slice(2));
+��+�����^r�춻�
--- src/lib/cli.js
+++ src/lib/cli.js
@@ -1,14 +1,1 @@
-import { Command } from 'commander';
-import { createInterface } from 'readline/promises';
-import { stdin as input, stdout as output } from 'process';
-import { TaskManager } from './tasks.js';
-
-export class CLI {
-  constructor(tasksFile) {
-    this.taskManager = new TaskManager(tasksFile);
-    this.program = new Command();
-    this.setupCommands();
-  }
-  
-  
-      
+r�춷�vX�
