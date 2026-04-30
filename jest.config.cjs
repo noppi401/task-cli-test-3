fe13d349b805b6b103b95875296248c977d5afc3
noppi401 <noppi623@gmail.com>
2026-05-01 02:07:17 +0900

    fix: address validation and test coverage review comments

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,1 @@
-module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.json' }]
-  }
-};
+��n�籦��
