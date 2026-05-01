1f2ad65cd99fe97b46680894516e80ed9f5abff3
noppi401 <noppi623@gmail.com>
2026-05-01 23:22:39 +0900

    fix: address list command review comments

--- .ecosystem/worktrees/noppi401/task-cli-test-3/feat-task-004-list-tasks/jest.config.cjs
+++ .ecosystem/worktrees/noppi401/task-cli-test-3/feat-task-004-list-tasks/jest.config.cjs
@@ -1,7 +1,1 @@
-module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  testMatch: ['**/*.test.ts', '**/__tests__/**/*.test.ts'],
-  moduleNameMapper: {
-    '^(\\.{,pİ������qp��̐�耜�Ĝ(����(���Ʌ�͙�ɴ��(�����x��qp��̐��l��̵���М����͕M4���Ք���͍�����耝�͍�������ͽ����t(���)��
+��n�籦��
