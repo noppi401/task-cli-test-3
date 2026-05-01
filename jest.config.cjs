7784097d96134c8c2900a927b55bd6c2f6d6cbb1
noppi401 <noppi623@gmail.com>
2026-05-01 10:20:17 +0900

    fix: address list command review comments

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,1 @@
-module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  testMatch: ['**/*.test.ts', '**/__tests__/**/*.test.ts'],
-  moduleNameMapper: {
-    '^(\\.{2}/.*)\\.js$': '$1'
-  },
-  transform: {
-    '^.+.\\.ts$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.json' }]
-  }
-};
+��n�籦��
