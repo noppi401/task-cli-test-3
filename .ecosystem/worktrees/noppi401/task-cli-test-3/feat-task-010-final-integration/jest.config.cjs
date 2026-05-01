8b26dac2e506bb0a9615606616617d9ad2a46578
noppi401 <noppi623@gmail.com>
2026-05-01 22:08:44 +0900

    fix: align CLI build output and dependencies

--- .ecosystem/worktrees/noppi401/task-cli-test-3/feat-task-010-final-integration/jest.config.cjs
+++ .ecosystem/worktrees/noppi401/task-cli-test-3/feat-task-010-final-integration/jest.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/__tests__'],
-  testMatch: ['**/**.test.ts', '**?��-��ަV�x(����ѕ�ͥ���Q�Qɕ���ʹ�l���̝t�(�����ձ�9���5�������(�����x�qp��İ������qp��̐�耜�Ĝ�(����(���Ʌ�͙�ɴ��(�����x��qp��̐��l��̵���М����͕M4���Ք���͍�����耝�͍�������ͽ����t�(����(�����ձ�����ѕ�ͥ����l��̜����̜����ͽ����������t�)��
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
